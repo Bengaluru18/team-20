ef878440f34a932eec8b3a1247aa5f9d87d08247
Purusharth07 <puruarora97@gmail.com>
2018-07-07 20:38:03 +0530

    addedd extra files

--- ssk_webapp/src/signin/SignUpForm.js
+++ ssk_webapp/src/signin/SignUpForm.js
@@ -7,8 +7,10 @@ class SignUpForm extends Component {
 
         this.state = {
             email: '',
+            department_name: '',
             password: '',
             name: '',
+            number: '',
             hasAgreed: false
         };
 
@@ -42,12 +44,20 @@ class SignUpForm extends Component {
                 <input type="text" id="name" className="FormField__Input" placeholder="Enter your full name" name="name" value={this.state.name} onChange={this.handleChange} />
               </div>
               <div className="FormField">
+                <label className="FormField__Label" htmlFor="number">Mobile Number</label>
+                <input type="number" id="number" className="FormField__Input" placeholder="Mobile Number" name="number" value={this.state.number} onChange={this.handleChange} />
+              </div>
+              <div className="FormField">
                 <label className="FormField__Label" htmlFor="password">Password</label>
                 <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
               </div>
               <div className="FormField">
-                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
+                <label className="FormField__Label" htmlFor="department_name">Department</label>
+                <input type="text" id="department_name" className="FormField__Input" placeholder="Enter your Department" name="department_name" value={this.state.department_name} onChange={this.handleChange} />
+              </div>
+              <div className="FormField">
+                <label className="FormField__Label" htmlFor="email">Email Address</label>
+                <input type="email" id="email" className="FormField__Input" placeholder="Enter your Email" name="email" value={this.state.email} onChange={this.handleChange} />
               </div>
 
               <div className="FormField">
