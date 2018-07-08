2373d88348080e418462e2de3788004633749ac8
Aravind B.S <bs.aravind.97@gmail.com>
2018-07-08 10:47:46 +0530

    final API

--- ssk2.sql
+++ ssk2.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.16  Distrib 10.2.9-MariaDB, for osx10.11 (x86_64)
 --
--- Host: localhost    Database: ssk2
+-- Host: localhost    Database: ssk3
 -- ------------------------------------------------------
 -- Server version	10.2.9-MariaDB
 
@@ -23,17 +23,17 @@ DROP TABLE IF EXISTS `appointment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `appointment` (
-  `aid` varchar(20) NOT NULL,
-  `pid` varchar(30) DEFAULT NULL,
-  `sid` varchar(30) DEFAULT NULL,
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `sid` int(11) DEFAULT NULL,
+  `pid` int(11) DEFAULT NULL,
   `timing` datetime DEFAULT NULL,
-  `status` varchar(30) DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`aid`),
-  KEY `pid` (`pid`),
   KEY `sid` (`sid`),
-  CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `patient` (`pid`),
-  CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`sid`) REFERENCES `specialist` (`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `pid` (`pid`),
+  CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `specialist` (`sid`),
+  CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `patient` (`pid`)
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,6 @@ CREATE TABLE `appointment` (
 
 LOCK TABLES `appointment` WRITE;
 /*!40000 ALTER TABLE `appointment` DISABLE KEYS */;
-INSERT INTO `appointment` VALUES ('1234','20170404','54678234','2018-03-24 03:45:00','approved'),('1235','20170409','54679876','2018-07-12 02:30:00','pending'),('1236','20170409','54887777','2018-06-15 02:30:00','done'),('1237','20170476','54887777','2018-07-15 02:30:00','pending'),('1238','20180202','54889976','2018-07-20 05:00:00','approved');
 /*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,14 +53,15 @@ DROP TABLE IF EXISTS `patient`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `patient` (
-  `pid` varchar(30) NOT NULL,
-  `pname` varchar(30) DEFAULT NULL,
-  `dob` date DEFAULT NULL,
-  `parent_name` varchar(30) DEFAULT NULL,
-  `phno` varchar(30) DEFAULT NULL,
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `pname` varchar(20) DEFAULT NULL,
+  `dob` varchar(30) DEFAULT NULL,
+  `phno` varchar(10) DEFAULT NULL,
+  `parent_name` varchar(20) DEFAULT NULL,
   `address` varchar(30) DEFAULT NULL,
+  `comment` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`pid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `patient` (
 
 LOCK TABLES `patient` WRITE;
 /*!40000 ALTER TABLE `patient` DISABLE KEYS */;
-INSERT INTO `patient` VALUES ('20170404','Abdul Kumar','2002-04-05','Faizal Kumar','8928765445','2nd cross,RR nagar,Banglore'),('20170409','Lakskmi Shetty','2011-12-04','Niranjan shetty','8912345678','6th cross,Jaynagar,Mysore'),('20170476','Lakshman Hegde','2009-06-07','Srinath Hegde','8928767898','12nd cross,Kuvampunagar,Mysore'),('20180202','Ram Prasad','2001-09-08','Uttam Prasad','8926589845','1st cross,Jaynagar,Banglore');
+INSERT INTO `patient` VALUES (1,'Chandra Murthy','2016-09-08','9876456787','Krishna Murthy','12th cross, RR nagar, Mysuru',''),(2,'Afzal K','2016-09-01','9876498787','Faizal K','11th cross, TK nagar, Mysuru',''),(4,'Jyothi Pai','2017-04-04','9806498787','Pradeep Pai','1st cross, SA layout, Banglore',''),(5,'Ramesh shetty','2016-09-04','9456498787','Sathwik shetty','5th cross, TK layout, Mysuru','');
 /*!40000 ALTER TABLE `patient` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,12 +82,12 @@ DROP TABLE IF EXISTS `specialist`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `specialist` (
-  `sid` varchar(30) NOT NULL,
-  `sname` varchar(30) DEFAULT NULL,
-  `dept_name` varchar(40) DEFAULT NULL,
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `sname` varchar(20) DEFAULT NULL,
+  `dept_name` varchar(30) DEFAULT NULL,
   `phno` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,33 +96,35 @@ CREATE TABLE `specialist` (
 
 LOCK TABLES `specialist` WRITE;
 /*!40000 ALTER TABLE `specialist` DISABLE KEYS */;
-INSERT INTO `specialist` VALUES ('54678234','Prashanth Kumar','Paediatrics','7569086758'),('54679876','Balkrishna Hedge','Orthopaedics','9900886758'),('54885467','Shraddha Kapoor','STnA','7799009958'),('54887000','Pradeep Pai','ENT','7799009958'),('54887777','Akhila Bharti','Dentist','9999009958'),('54889976','Roopa Prakash','Psycology','9909876758');
+INSERT INTO `specialist` VALUES (2,'Prashanth Kumar','Paediatrics','7456888888'),(3,'Punitha Bargi','Psycology','7477778888'),(4,'Balakrishne Gowda','Orthopaedics','7456879456'),(6,'Pulkit Sinha','Dental','7476689888');
 /*!40000 ALTER TABLE `specialist` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `unavailability`
+-- Table structure for table `unavail`
 --
 
-DROP TABLE IF EXISTS `unavailability`;
+DROP TABLE IF EXISTS `unavail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `unavailability` (
-  `sid` varchar(30) DEFAULT NULL,
-  `startdate` datetime DEFAULT NULL,
-  `enddate` datetime DEFAULT NULL,
+CREATE TABLE `unavail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sid` int(11) DEFAULT NULL,
+  `start_date` datetime DEFAULT NULL,
+  `end_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `sid` (`sid`),
-  CONSTRAINT `unavailability_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `specialist` (`sid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `unavail_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `specialist` (`sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `unavailability`
+-- Dumping data for table `unavail`
 --
 
-LOCK TABLES `unavailability` WRITE;
-/*!40000 ALTER TABLE `unavailability` DISABLE KEYS */;
-/*!40000 ALTER TABLE `unavailability` ENABLE KEYS */;
+LOCK TABLES `unavail` WRITE;
+/*!40000 ALTER TABLE `unavail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `unavail` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -134,4 +136,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-07 17:20:22
+-- Dump completed on 2018-07-08  2:38:57
